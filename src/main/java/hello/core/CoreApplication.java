bd163d0b236a799f588cb49cb9aa902235529624
sinbpark <pjhwin1@naver.com>
2022-09-28 21:01:20 +0900

    비즈니스 요구사항과 설계 정리 회원, 주문과 할인정책 회원 도메인 설계 이미지 추가

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,10 +3,10 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //스프링 부트의 가장 기본적인 설정을 선언해줌
 public class CoreApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //스프링 부트는 main 메소드가 선언된 클래스를 기준으로 실행됨
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
